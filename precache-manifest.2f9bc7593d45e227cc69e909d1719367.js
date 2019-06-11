c3be7a074a77d51fc7189c962f4df9cd7fe1ec3c
Oleg Lykasov <oleglykasov@gmail.com>
2019-06-11 18:15:57 +0300

    Updates

---
+++ precache-manifest.2f9bc7593d45e227cc69e909d1719367.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e91b240dc2cbbdad39b5041ebebd1436",
+    "url": "https://superol3g.github.io/coding-challenge-frontend-react/index.html"
+  },
+  {
+    "revision": "0c78cf92cb75be119346",
+    "url": "https://superol3g.github.io/coding-challenge-frontend-react/static/css/2.33bbdc9a.chunk.css"
+  },
+  {
+    "revision": "0c78cf92cb75be119346",
+    "url": "https://superol3g.github.io/coding-challenge-frontend-react/static/js/2.d84ddacd.chunk.js"
+  },
+  {
+    "revision": "968cb672ec6f029336bb",
+    "url": "https://superol3g.github.io/coding-challenge-frontend-react/static/js/main.1acdca94.chunk.js"
+  },
+  {
+    "revision": "f2f9c39510e581abdd45",
+    "url": "https://superol3g.github.io/coding-challenge-frontend-react/static/js/runtime~main.51ec2c90.js"
+  },
+  {
+    "revision": "b104a8d258584640b916de9db70683b3",
+    "url": "https://superol3g.github.io/coding-challenge-frontend-react/static/media/bycicle.b104a8d2.svg"
+  },
+  {
+    "revision": "aba6572da1ac8b8d7a18cdfbbbfa1b0c",
+    "url": "https://superol3g.github.io/coding-challenge-frontend-react/static/media/logo.aba6572d.svg"
+  }
+]);
